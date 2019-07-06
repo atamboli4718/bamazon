ffc7835cba5067960ec0212c8630310e5c3006ca
Alicia Tamboli <atamboli4718@gmail.com>
2019-07-06 16:48:48 -0400

    created table and database successfully showing, have first two questions set up

--- bamazonCustomercopy.js
+++ bamazonCustomercopy.js
@@ -1,56 +1,73 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+//cli table 
+var table = new Table({
+  head: ['ID', 'NAME', 'PRICE'],
+  colWidths: [8, 12, 8],
+  colAligns: ['middle', 'middle', 'middle']
+});
 
 // creates the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",                                     
-    
-    // Your port; if not 3306                        
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "432ograL!",
-    database: "bamazon_db"
-  });
+  host: "localhost",
 
-  connection.connect(function(err) {
+  port: 3306,
+
+  user: "root",
+
+  // Your password
+  password: "432ograL!",
+  database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  //if connection is successful, display connection id
+  console.log("connected as id " + connection.threadId)
+  displayItems();
+});
+
+function displayItems() {
+  connection.query("SELECT * FROM products", function (err, results) {
     if (err) throw err;
-    //if connection is successful, display connection id
-    console.log("connected as id " + connection.threadId)
-    displayItems();
-  }); 
-
-  function displayItems(){
-      connection.query("SELECT * FROM products", function(err,results){
-          if (err) throw err;
-          inquirer.prompt([
-              {
-                  name: "products available",
-                  type: "rawlist",
-                  choices: function(){
-                      var productOptions;
-                      for (var i=0;i>results.length;i++){
-                        productOptions.push(results[i].item_id);
-                        //productOptions.push(results[i].product_name);
-                        //productOptions.push(results[i].price);
-                      }
-                      return productOptions;
-                  },
-                  message: "what is the id of the item you would like to purchase?"
-              }
-          ]).then(function(answer){
-              var chosenItem;
-              for (var i=0; i<results.length; i++){
-                  if (results[i].product_name ===answer.choice) {
-                      chosenItem = results[i];
-                      console.log('user chose: ' +chosenItem);
-                  }
-              }
-
-          })
+    for (var i = 0; i < results.length; i++) {
+      table.push(
+        [results[i].item_id,
+          results[i].product_name,
+          results[i].price
+        ])
+    }
+    console.log(table.toString());
+    inquirer
+      .prompt([{
+        name: "idSelection",
+        type: "number",
+        message: "what is the id of the item you would like to purchase?"
+      }, ]).then(function (idSelection) {
+        var chosenItem = idSelection
+        console.log(chosenItem);
+        howManyFun();
       })
-  }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+  })
+}
+
+function howManyFun() {
+  inquirer
+    .prompt([{
+      name: "quantitySelection",
+      type: "number",
+      message: "How many would you like to purchase?"
+    }, ]).then(function (idSelection) {
+      connection.query("SELECT stock_quantity FROM products WHERE item_id = ?", [idSelection],
+        function (err, results) {
+          if (err) throw err;
+          if (idSelection > results) {
+            console.log("it's yours!")
+          }
+
+        })
+
+    })
+}
